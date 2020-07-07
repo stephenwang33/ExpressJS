35a236535b3b27a270e15800d8789d776c526433
stephenwang33 <63862679+stephenwang33@users.noreply.github.com>
2020-07-07 14:17:31 -0400

    add swagger document

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var cors = require('cors');
 //var mssql = require('mssql');
 var path = require('path');
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+//const swaggerDocument = require('./swagger.json');
+
+
 const autoArray = [
    {id: 11,
    name: 'Charles',
@@ -27,6 +32,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //api router
 app.use('/api/autos', require('./routes/autos'));
+
+const swaggerDefinition = {
+	swaggerDefinition: {
+		info: {
+			title: "Stephen Node JS api",
+			description: "My sample of autos api",
+			contact: {
+				name: "Stephen Wang"
+			}
+		},
+		servers: [
+			"http://localhost:5000"
+		]
+	},
+	apis: ["index.js", "./routes/autos.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerDefinition);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+
+
 
 app.listen(PORT, 
 	()=> console.log('Express server started on port ' + PORT)
--- routes/Autos.js
+++ routes/Autos.js
@@ -10,11 +10,39 @@ const auto = new AutoInfo(1, 'chevrolet', 'two', 'hatchback', 'three', 48, 5100,
 const autos = auto.getAutos();
 
 //list all autos
+/**
+ * @swagger
+ * /api/autos:
+ *   get:
+ *     description: List all Autos.
+ *     responses:              
+ *       '200': 
+ *         description: A success response.
+ *
+ */
 router.get("/", (req,res) => {
 	res.json(autos);
 });
 
 //get auto by id
+/**
+ * @swagger
+ * /api/autos/{id}:
+ *   get:
+ *     description: Get a Auto by id.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         type: integer
+ *         required: true;
+ *         description: Numeric ID to get an Auto 
+ *     responses:              
+ *       '200': 
+ *         description: A success response call.
+ *       '400':
+ *         description: AutoInfo is not found 
+ *
+ */
 router.get("/:id", (req,res) => {
 	const isFound = autos.some(auto => auto.id === parseInt(req.params.id));
 	if (isFound) {
@@ -26,6 +54,16 @@ router.get("/:id", (req,res) => {
 });
 
 //Create new Auto
+/**
+ * @swagger
+ * /api/autos:
+ *   post:
+ *     description: post a new Auto.
+ *     responses:              
+ *       '200': 
+ *         description: A success response.
+ *
+ */
 router.post('/', (req, res) => {
 	res.send(req.body);
 });
